e1d976c9f0786826e810f1517434c3fea73d7169
Alex Brazier <alex.brazier@red-badger.com>
2022-08-03 11:51:07 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css'
+
+const logo = require('./styles/small-header.svg')
 
 function App() {
   return (
     <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+const Header = () => {
+  return (
+    <div className="Header">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
