e44e6665b2444a00f3df8016839bd13b2eef4f1c
Alex Brazier <alex.brazier@red-badger.com>
2022-08-05 16:23:35 +0100

    refactor StepList to use arrow component

--- src/components/StepList.tsx
+++ src/components/StepList.tsx
@@ -1,107 +1,103 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Step from './Step';
 import { StepObject, TestCaseObject } from '../modules/TestCase';
 import '../styles/Step.css';
 
-interface StepListProps {
+type StepListProps = {
   selectedTestCase: TestCaseObject;
   updateSelectedTestCase: (testCase: TestCaseObject) => void;
 }
 
 // list of steps to run a test case
-class StepList extends Component<StepListProps> {
-  state = {
-    newStep: ''
-  }
+const StepList = ({selectedTestCase, updateSelectedTestCase}: StepListProps) => {
+  const [stepEntry, setStepEntry] = useState('')
 
-  addStep = (step: string) => {
+  const addStep = (step: string) => {
     let newStep = new StepObject(
-      this.props.selectedTestCase.steps.length, // TOFIX - will break when deleting steps
+      selectedTestCase.steps.length, // TOFIX - will break when deleting steps
       step
     );
-    let newStepList = [...this.props.selectedTestCase.steps, newStep]
+    let newStepList = [...selectedTestCase.steps, newStep]
 
-    this.props.updateSelectedTestCase({
-      ...this.props.selectedTestCase,
+    updateSelectedTestCase({
+      ...selectedTestCase,
       steps: newStepList
     });
   }
 
-  deleteStep = (id: number) => { 
-    let index = this.props.selectedTestCase.steps.findIndex( s => {return s.id === id});
-    let newStepList = [...this.props.selectedTestCase.steps.slice(0, index), ...this.props.selectedTestCase.steps.slice(index+1)];
+  const deleteStep = (id: number) => { 
+    let index = selectedTestCase.steps.findIndex( s => {return s.id === id});
+    let newStepList = [...selectedTestCase.steps.slice(0, index), ...selectedTestCase.steps.slice(index+1)];
 
-    this.props.updateSelectedTestCase({
-      ...this.props.selectedTestCase,
+    updateSelectedTestCase({
+      ...selectedTestCase,
       steps: newStepList
     })
   }
 
-  updateStep = (id: number, newName: string) => {
+  const updateStep = (id: number, newName: string) => {
     let newStep = new StepObject(
       id,
       newName
     )    
-    let index = this.props.selectedTestCase.steps.findIndex( s => {return s.id === id});
-    let newStepList = [...this.props.selectedTestCase.steps.slice(0, index), newStep, ...this.props.selectedTestCase.steps.slice(index+1)];
+    let index = selectedTestCase.steps.findIndex( s => {return s.id === id});
+    let newStepList = [...selectedTestCase.steps.slice(0, index), newStep, ...selectedTestCase.steps.slice(index+1)];
 
-    this.props.updateSelectedTestCase({
-      ...this.props.selectedTestCase,
+    updateSelectedTestCase({
+      ...selectedTestCase,
       steps: newStepList
     })
   }
 
-  handleUserInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ newStep: event.target.value });
+  const handleUserInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStepEntry(event.target.value);
   }
 
-  handleKeyDown = (event: React.KeyboardEvent) => {
+  const handleKeyDown = (event: React.KeyboardEvent) => {
     if (event.key === 'Enter' || event.key === 'Tab') {
-      if (this.state.newStep !== '') {
+      if (stepEntry !== '') {
         event.preventDefault();
-        this.addStep(this.state.newStep);
-        this.setState({ newStep: '' });
+        addStep(stepEntry);
+        setStepEntry('');
       }
     }
   }
 
-  handleBlur = () => {
-    if (this.state.newStep !== '') {
-      this.addStep(this.state.newStep);
+  const handleBlur = () => {
+    if (stepEntry !== '') {
+      addStep(stepEntry);
     }
-    this.setState({ newStep: '' });
+    setStepEntry('');
   }
 
-  render() {
-    const stepsToRender = this.props.selectedTestCase.steps.map( step => 
-      <Step
-        key={step.id}
-        step={step}
-        deleteStep={this.deleteStep}
-        updateStep={this.updateStep}
-        disabled={this.props.selectedTestCase.disabled}
-      />
-    );
+  const stepsToRender = selectedTestCase.steps.map( step => 
+    <Step
+      key={step.id}
+      step={step}
+      deleteStep={deleteStep}
+      updateStep={updateStep}
+      disabled={selectedTestCase.disabled}
+    />
+  );
 
-    return(
-      <div data-testid="step-list" className="Step-list-container">
-        <span className="Label">Steps</span>
-        <span className="Step-list">
-          {stepsToRender}
-          <input
-            data-testid="step-new"
-            className="Step-input"
-            placeholder="Enter new step..."
-            value={this.state.newStep}
-            onChange={this.handleUserInput}
-            onKeyDown={this.handleKeyDown}
-            onBlur={this.handleBlur}
-            disabled={this.props.selectedTestCase.disabled}
-          />
-        </span>
-      </div>
-    );
-  }
+  return(
+    <div data-testid="step-list" className="Step-list-container">
+      <span className="Label">Steps</span>
+      <span className="Step-list">
+        {stepsToRender}
+        <input
+          data-testid="step-new"
+          className="Step-input"
+          placeholder="Enter new step..."
+          value={stepEntry}
+          onChange={handleUserInput}
+          onKeyDown={handleKeyDown}
+          onBlur={handleBlur}
+          disabled={selectedTestCase.disabled}
+        />
+      </span>
+    </div>
+  );
 }
 
 export default StepList;
