a3c4ad73db491bb9cae23b3e6bd92c4554097084
Alex Brazier <alex.brazier@red-badger.com>
2022-09-07 16:38:16 +0100

    get initial state from API

--- src/atoms/TestCaseListState.tsx
+++ src/atoms/TestCaseListState.tsx
@@ -1,13 +1,36 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
+import axios from 'axios';
 import { blankTestCase, TestCaseObject } from "../modules/TestCase";
 
-const defaultTestCases: TestCaseObject[] = [
-  blankTestCase()
-];
+const baseUrl = "http://localhost:3001"
+
+const getTestCaseList = selector({
+  key: 'GetTestCaseList',
+  get: async () => {
+    let testCases: TestCaseObject[] = [];
+    const res = await axios.get(`${baseUrl}/api/testCases`)
+
+    console.log(res)
+    if (res.data.data.length > 0) {
+      testCases = res.data.data.map((testCase: TestCaseObject) =>
+        new TestCaseObject(
+          testCase.key,
+          testCase.summary,
+          testCase.description,
+          testCase.steps,
+          testCase.tags
+        )
+      );
+    }
+    testCases.push(blankTestCase());
+    console.log(testCases);
+    return testCases;
+    }
+});
 
 const testCaseListState = atom<TestCaseObject[]>({
   key: 'testCaseList',
-  default: defaultTestCases
+  default: getTestCaseList
 })
 
 export default testCaseListState;
