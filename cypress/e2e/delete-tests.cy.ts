e5dff109bc64711daf1cac01223f91d06038da52
Alex Brazier <alex.brazier@red-badger.com>
2022-10-03 17:43:50 +0100

    add and delete tests

--- cypress/e2e/delete-tests.cy.ts
+++ cypress/e2e/delete-tests.cy.ts
@@ -1,4 +1,4 @@
-it('mock single test case', () => {
+it('delete using delete button', () => {
   cy.mockGetTestCaseList('get/delete-test-case');
   cy.mockDeleteTestCase('delete/success');
   cy.visit('/');
@@ -13,7 +13,23 @@ it('mock single test case', () => {
   cy.getBySelContaining('tag', 'one-delete').should('not.exist');
   cy.getBySelContaining('tag', 'test-delete').should('exist');
 
-  cy.delete('test-case', 'first test');
-  cy.getBySelContaining('test-case', 'first test').should('not.exist');
-  cy.getBySelContaining('test-case', 'second test to delete').should('exist');
+  cy.delete('test-case', 'second test to delete');
+  cy.getBySelContaining('test-case', 'first test').should('exist');
+  cy.getBySelContaining('test-case', 'second test to delete').should(
+    'not.exist'
+  );
+});
+
+it('delete by clearing summary field', () => {
+  cy.mockGetTestCaseList('get/delete-test-case');
+  cy.mockDeleteTestCase('delete/success');
+  cy.visit('/');
+  cy.wait('@getTestCases');
+  cy.selectTestCase('second test to delete').clear().type('{enter}');
+
+  cy.getBySelContaining('test-case', '').its('length').should('eq', 2);
+  cy.getBySelContaining('test-case', 'first test').should('exist');
+  cy.getBySelContaining('test-case', 'second test to delete').should(
+    'not.exist'
+  );
 });
--- cypress/e2e/tests.cy.ts
+++ cypress/e2e/tests.cy.ts
@@ -10,3 +10,26 @@ it('mock single test case', () => {
   cy.getBySel('tag').first().contains('one');
   cy.getBySel('tag').contains('test');
 });
+
+it('add new test case using Enter', () => {
+  cy.mockGetTestCaseList('get/single-test-case');
+  cy.visit('/');
+  cy.wait('@getTestCases');
+
+  cy.getBySel('test-case-input').last().click().type('test input{enter}');
+
+  cy.selectTestCase('test input').should('exist');
+  cy.getBySel('test-case-input').last().should('have.text', '');
+});
+
+it('add new test case using Save', () => {
+  cy.mockGetTestCaseList('get/single-test-case');
+  cy.visit('/');
+  cy.wait('@getTestCases');
+
+  cy.getBySel('test-case-input').last().click().type('test input');
+  cy.getBySel('Save').click();
+
+  cy.selectTestCase('test input').should('exist');
+  cy.getBySel('test-case-input').last().should('have.text', '');
+});
