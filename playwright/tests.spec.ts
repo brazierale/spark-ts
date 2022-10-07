8f1da451b3aa91fb82058d90438c6fcf0b221903
Alex Brazier <alex.brazier@red-badger.com>
2022-10-07 15:54:53 +0100

    mock get test cases

--- playwright/tests.spec.ts
+++ playwright/tests.spec.ts
@@ -1,6 +1,37 @@
 import { test, expect } from '@playwright/test';
 
 test('load test case', async ({ page }) => {
+  let body = JSON.stringify({
+    success: true,
+    data: [
+      {
+        tags: ['one', 'test'],
+        steps: [
+          {
+            id: 1,
+            name: 'step one',
+          },
+          {
+            id: 2,
+            name: 'step two',
+          },
+        ],
+        key: '84dbe4ea-f98f-4d0a-bdfd-ab7173070f55',
+        sortId: 1,
+        summary: 'first test',
+        description: 'first test description',
+        __v: 0,
+      },
+    ],
+  });
+
+  await page.route('**/api/testCases', (route) =>
+    route.fulfill({
+      status: 200,
+      body: body,
+    })
+  );
+
   await page.goto('http://localhost:3000');
 
   await expect(page).toHaveTitle(/Spark/);
