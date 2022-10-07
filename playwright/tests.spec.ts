7ffe5b5d20a18832fa74f3b3442d9eae6a47c2e4
Alex Brazier <alex.brazier@red-badger.com>
2022-10-07 17:56:18 +0100

    add test case test

--- playwright/tests.spec.ts
+++ playwright/tests.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from './fixtures';
 
-test('load test case', async ({ singleTestPage }) => {
+test('display test case details', async ({ singleTestPage }) => {
   await expect(
     singleTestPage.locator('data-testid=description-input')
   ).toHaveText(' first test description');
@@ -15,5 +15,21 @@ test('load test case', async ({ singleTestPage }) => {
   ).toBeVisible();
   await expect(
     singleTestPage.locator('data-testid=tag', { hasText: 'test' })
+  ).toBeVisible();
+});
+
+test('add new test case using Enter', async ({ singleTestPage }) => {
+  await singleTestPage.route('**/api/testCases/*', (route) =>
+    route.fulfill({ path: './playwright/single-test-case.json' })
+  );
+
+  await singleTestPage
+    .locator('data-testid=test-case-new')
+    .click.type('test input')
+    .press('Enter');
+
+  await expect(singleTestPage('data-testid=test-case-new')).toHaveText('');
+  await expect(
+    singleTestPage.locator('data-testid=test-case').hasText('test input')
   ).toBeVisible();
 });
