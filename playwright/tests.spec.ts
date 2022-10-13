a39d8a722b0a172873438c2325f47be021b06fbe
Alex Brazier <alex.brazier@red-badger.com>
2022-10-13 17:39:44 +0100

    update request test working

--- playwright/tests.spec.ts
+++ playwright/tests.spec.ts
@@ -33,3 +33,52 @@ test('add new test case using Enter', async ({ singleTestPage }) => {
     singleTestPage.locator('data-testid=test-case', { hasText: 'test input' })
   ).toBeVisible();
 });
+
+test('add new test case using Save', async ({ singleTestPage }) => {
+  await singleTestPage.route('**/api/testCases/*', (route) =>
+    route.fulfill({ path: './playwright/single-test-case.json' })
+  );
+
+  await singleTestPage.locator('data-testid=test-case-new').fill('test input');
+  await singleTestPage.locator('data-testid=save').click();
+
+  await expect(singleTestPage.locator('data-testid=test-case-new')).toHaveText(
+    ''
+  );
+  await expect(
+    singleTestPage.locator('data-testid=test-case', { hasText: 'test input' })
+  ).toBeVisible();
+});
+
+test('edit and save test case', async ({ singleTestPage }) => {
+  await singleTestPage.route('**/api/testCases/*', (route) =>
+    route.fulfill({ path: './playwright/single-test-case.json' })
+  );
+
+  singleTestPage
+    .locator('data-testid=test-case', { hasText: 'first test' })
+    .click();
+  await singleTestPage.locator('data-testid=summary').click();
+  await singleTestPage.locator('data-testid=summary').type(' updated');
+  await singleTestPage.locator('data-testid=description').click();
+  await singleTestPage.locator('data-testid=description').type(' updated');
+  await singleTestPage
+    .locator('data-testid=step', { hasText: 'step one' })
+    .click();
+  await singleTestPage
+    .locator('data-testid=step', { hasText: 'step one' })
+    .type(' updated');
+    
+  singleTestPage.on('request', (request) => {
+    expect(request.postDataJSON().update).toEqual({
+      description: 'first test description updated',
+      steps: [
+        { id: 1, name: 'step one updated' },
+        { id: 2, name: 'step two' },
+      ],
+      summary: 'first test updated',
+      tags: ['one', 'test'],
+    });
+  });
+  await singleTestPage.locator('data-testid=save').click();
+});
